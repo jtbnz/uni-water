6aa33c34597b15f9a26c90dd14dd44cdab2cd1e0
jtbnz <57672422+jtbnz@users.noreply.github.com>
2024-09-13 09:29:23 +1200

    change from unlimited to loop

--- digital_relay.py
+++ digital_relay.py
@@ -8,13 +8,14 @@ Board().begin()  # Initialize the UNIHIKER
 
 led = Pin(Pin.P22, Pin.OUT)  # Initialize the pin as an output pin
 
-while True:
-    # led.value(1)  # Set the pin output to high level - Method 1
-    led.write_digital(1)  # Set the pin output to high level - Method 2
-    print("1")  # Print message to the terminal
-    time.sleep(1)  # Wait for 1 second to maintain the state
+count = 0
+while count < 10:
+    led.write_digital(1)
+    print("1")
+    time.sleep(1)
 
-    # led.value(0)  # Set the pin output to low level - Method 1
-    led.write_digital(0)  # Set the pin output to low level - Method 2
-    print("0")  # Print message to the terminal
-    time.sleep(1)  # Wait for 1 second to maintain the state
+    led.write_digital(0)
+    print("0")
+    time.sleep(1)
+
+    count += 1
